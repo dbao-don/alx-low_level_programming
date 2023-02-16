f57dd311c16689c00a7c5be986bd98e207577241
Donbosco Otunga <donotunga@gmail.com>
2023-02-16 11:38:11 -0800

    Ammending the comments according to betty styles

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Use the function puts
  * You are not allowed to use printf
  * Return: 0
- *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry Point
- *
- * Use the function printf
- * You are not allowed to use the function puts
- * Your program should compile without warning when 
- * using the -Wall gcc option
- * return: 0
- * 
+ * main - A program that prints a line using the printf function
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
 }
