1fdb7b38be1df4b3a7fa09f02378a54a9c345fc4
Donbosco Otunga <donotunga@gmail.com>
2023-02-17 13:44:56 -0800

    Edited file 2 print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Programs that prints the
+ * alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
