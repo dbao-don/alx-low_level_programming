b45978df85c3316b7f933330acd0e701c7696e23
Donbosco Otunga <donotunga@gmail.com>
2023-02-17 15:56:54 -0800

    Edited file 6 print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Program that prints all
+ * single digit numbers
+ * of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
