4e7c58817150583fe982d68e2be66457ca1b1255
Donbosco Otunga <donotunga@gmail.com>
2023-02-17 15:47:37 -0800

    Edited file 5 print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Program that prints all
+ * single digit numbers
+ * of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
