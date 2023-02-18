61444ccb96e54e9cd6876a5b9da88ba05fbef02d
Donbosco Otunga <donotunga@gmail.com>
2023-02-17 16:39:30 -0800

    Edited file 8 print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Program that prints all
+ * the numbers of base 16
+ * in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 97;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	while (j <= 102)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
