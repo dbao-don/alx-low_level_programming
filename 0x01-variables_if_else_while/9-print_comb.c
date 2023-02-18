6292bc9953a71fd42e512ec263efe08dce2cf14f
Donbosco Otunga <donotunga@gmail.com>
2023-02-17 17:16:41 -0800

    Edited file 9 print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Program that prints all
+ * possible combinations of
+ * single digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
