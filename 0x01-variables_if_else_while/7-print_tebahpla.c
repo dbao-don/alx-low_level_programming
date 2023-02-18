36f5a7f731e0b157e67d951d775cda913151cf31
Donbosco Otunga <donotunga@gmail.com>
2023-02-17 16:10:15 -0800

    Edited file 7 print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Programs that prints the
+ * alphabet in lowercase
+ * in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
