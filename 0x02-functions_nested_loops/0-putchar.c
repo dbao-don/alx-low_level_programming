674599572e3ecffbcd69fd452a9427d610a81f17
Donbosco Otunga <donotunga@gmail.com>
2023-02-22 01:17:36 -0800

    Edited 0-putchar.c file code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
 #include "main.h"
 /**
  * main - Entry Point
+ * A program that prints
+ * _putchar
  * Return:0
  */
 int main(void)
 {
+	char c[] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
