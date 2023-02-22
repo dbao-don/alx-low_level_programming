2fe3691727eea0993b4129eb10c19b6fe41586bc
Donbosco Otunga <donotunga@gmail.com>
2023-02-22 11:39:32 -0800

    Added 0x03-debugging project folder

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,49 @@
 #include "main.h"
 /**
- * main - Entry Point
- * Return:0
+ * jack_bauer - a function that
+ * prints every mind of the day.
+ * Return: void
  */
-int main(void)
+void jack_bauer(void)
 {
-	return (0);
+	int i;
+
+	while (i < 24)
+	{
+		int j;
+
+		while (j < 60)
+		{
+			if (i < 10 && j < 10)
+			{
+				_putchar(48);
+				_putchar('0' + i);
+				_putchar(':');
+				_putchar(48);
+				_putchar('0' + j);
+			}
+			else if (i < 10 && j >= 10)
+			{
+				_putchar(48);
+				_putchar('0' + i);
+				_putchar(':');
+				_putchar('0' + j);
+			}
+			else if (i >= 10 && j < 10)
+			{
+				_putchar('0' + i);
+				_putchar(':');
+				_putchar(48);
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar('0' + i);
+				_putchar(':');
+				_putchar('0' + j);
+			}
+			j++;
+		}
+		i++;
+	}
 }
