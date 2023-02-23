c37aed52dd23484567c53dec13bb55dd6aa01ad2
Donbosco Otunga <donotunga@gmail.com>
2023-02-22 17:13:14 -0800

    Edited file 102-fibonacci.c code

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,9 +1,25 @@
 #include "main.h"
 /**
- * main - Entry Point
+ * main - prints the first
+ * 50 Fibonacci numbers
  * Return:0
  */
 int main(void)
 {
+	int i = 1, j = 1, k = 2, l;
+
+	printf("%d, %d, ", j, k);
+
+	while (i <= 48)
+	{
+		l = j + k;
+		printf("%d, ", l);
+		j = k;
+		k = l;
+		i++;
+	}
+	l = j + k;
+	printf("%d\n", l);
 	return (0);
 }
+
