8b91a05a325a0d713ccce92ab21d3dc60e7d0456
Donbosco Otunga <donotunga@gmail.com>
2023-02-22 16:01:00 -0800

    Edited file 101-natural.c code

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,22 @@
 #include "main.h"
 /**
- * main - Entry Point
+ * main - prints multiples
+ * of 3 or 5 under 1024
  * Return:0
  */
 int main(void)
 {
+	int i = 0;
+	int j = 1;
+
+	while (j < 1024)
+	{
+		if (j % 3 == 0 || j % 5 == 0)
+		{
+			i += j;
+		}
+		j++;
+	}
+	printf("%d\n", i);
 	return (0);
 }
