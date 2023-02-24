efc06f7b1077cc2591ca7f4677e6e7949becf52d
Donbosco Otunga <donotunga@gmail.com>
2023-02-24 09:09:57 -0800

    File 6-print_line.c code review 2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,13 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (n > 0)
 	{
 		while (i <= n)
 		{
 			_putchar('_');
 			i++;
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
