e97c1ab12f2e9ce82d6a3ec9f9f84b4f05651da1
Donbosco Otunga <donotunga@gmail.com>
2023-02-24 10:16:46 -0800

    File 6-print_line.c code review 5

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,8 +2,16 @@
 /**
  * print_diagonal - a function that
  * draw as a diagonal line
- * @n: value for triangle
+ * @n: number of times \ prints
  * Return: void
  */
 void print_diagonal(int n)
-{}
+{
+	int i;
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(' ');
+	}
+	_putchar(92);
+	_putchar('\n');
+}
