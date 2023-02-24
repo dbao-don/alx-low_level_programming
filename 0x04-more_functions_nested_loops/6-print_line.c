aa090afec15395692aaf9204febdb21b82409c39
Donbosco Otunga <donotunga@gmail.com>
2023-02-23 18:23:32 -0800

    Added 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - function that
+ * draws a straight line
+ * @n: length of line
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		_putchar(95);
+		i++;
+	}
+	_putchar('\n');
+}
