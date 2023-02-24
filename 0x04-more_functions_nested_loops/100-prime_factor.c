cf3e7e1b0d310c8474a6783944abf18f8a6b3529
Donbosco Otunga <donotunga@gmail.com>
2023-02-23 17:35:46 -0800

    Added advanced projects files

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - a C program that finds
+ * and prints the largerst prime
+ * factor of the number
+ * Return: 0
+ */
+int main(void)
+{
+	long num = 612852476143;
+	long factor = 2;
+
+	while (factor <= sqrt(num))
+	{
+		if (num % factor == 0)
+		{
+			num = num / factor;
+		}
+		else
+		{
+			factor++;
+		}
+	}
+	printf("%ld\n", num);
+	return (0);
+}
