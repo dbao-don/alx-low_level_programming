bdee3ad7df22ced178abbda550921a6df15bead2
Donbosco Otunga <donotunga@gmail.com>
2023-02-27 14:16:55 -0800

    Added 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - a funtion that prints
+ * half of a string
+ * @str: pointer of a string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (*(str + i) != '\0')
+		i++;
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+	for(; j < i; j++)
+		_putchar(*(str + j));
+	_putchar('\n');
+}
