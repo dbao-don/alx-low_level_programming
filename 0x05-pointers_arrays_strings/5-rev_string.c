99261266d4eff318189ba352b13f3c62cb1c569a
Donbosco Otunga <donotunga@gmail.com>
2023-02-28 03:26:00 -0800

    Reviewed code for file 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,20 +5,27 @@
  * @s: string pointer
  * Return: void
  */
+int _strlen(char *s);
 void rev_string(char *s)
 {
+	int a = _strlen(s);
+	char *start = s;
+	char *end = s + a - 1;
+	
+	while (start < end)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
+}
+int _strlen(char *s)
+{
 	int a = 0;
-	int b;
 
 	while (*(s + a) != '\0')
-	{
 		a++;
-	}
-	a = a - 1;
-
-	for (b = a; b >= 0 ; b--)
-	{
-		*(s + a) = *(s + b);
-		a--;
-	}
+	return (a);
 }
