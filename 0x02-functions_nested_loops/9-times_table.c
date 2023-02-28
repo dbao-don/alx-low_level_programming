73dccb261e08ff159b7c738814a097054a56e50a
Donbosco Otunga <donotunga@gmail.com>
2023-02-28 10:37:11 -0800

    Reviewed 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,18 +11,24 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			int k = i * j;
+			int k = i * (j);
 
-			if (k > 0 && k <= 9)
+			if (k > 9)
 			{
-				_putchar(' ');
-				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			else
 			{
+				if (j != 0 && k <= 9)
+					_putchar(' ');
+				_putchar(k + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
 				_putchar(' ');
 			}
-			_putchar(k + '0');
 		}
 		_putchar('\n');
 	}
