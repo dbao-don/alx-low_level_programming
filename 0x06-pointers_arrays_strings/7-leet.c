7f05ec322142d8a154c169cb499aa5f8d33489a9
Donbosco Otunga <donotunga@gmail.com>
2023-03-02 12:20:30 -0800

    Added 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - a function that encodes
+ * a string into 1337
+ * @s : pointer to string
+ * Return: char
+ */
+char *leet(char *s)
+{
+	char a[] = "aeotlAEOTL";
+	char b[] = "4307143071";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
+}
