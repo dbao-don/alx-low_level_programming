1ad97fa5e19cdaa517baef29edd94cef307923db
Donbosco Otunga <donotunga@gmail.com>
2023-03-06 12:13:36 -0800

    Added 2-strchr.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,8 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		*dest++ =  src[i];
-	}
+	char *ptr = dest;
+	while (n--)
+		*ptr++ = *src++;
 	return (dest);
 }
