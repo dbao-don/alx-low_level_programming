91f7686214fa5fcbf07f6f390307798ee1ee13dc
Donbosco Otunga <donotunga@gmail.com>
2023-03-08 09:56:06 -0800

    Reviewed code for 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int is_prime_minor(int n, int i);
 /**
  * is_prime_number - a function that returns 1 if
  * the input is prime and 0 if not.
@@ -7,14 +8,22 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1 )
 		return (0);
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-	return (1);
+	return (is_prime_minor(n, n - 1));
+}
+/**
+ * is_prime_minor - a recursive function that
+ * takes two argurnments and check if it is prime.
+ * @n: number to be checked
+ * @i: factor for checking
+ * Return: prime or no pime
+ */
+int is_prime_minor(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (is_prime_minor(n, i - 1));
 }
