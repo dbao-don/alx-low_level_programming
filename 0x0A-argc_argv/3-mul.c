a8b9f6304da7edd9a8c153e761ac9903d194715b
Donbosco Otunga <donotunga@gmail.com>
2023-03-10 14:35:18 -0800

    3-mul.c hotfix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	for (i = 1; i < argc && **argv != '\0'; i++)
+	for (i = 1; i < argc && argv[i] != NULL; i++)
 		mul *= atoi(argv[i]);
 	printf("%d\n", mul);
 	return (0);
