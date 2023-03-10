577b7bfcae5e63934832f989d3dba145b1a5ed65
Donbosco Otunga <donotunga@gmail.com>
2023-03-10 14:29:11 -0800

    Added file 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiples two numbers
+ * @argc: arguments counters
+ * @argv: pointer array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i = 0, mul = 1;
+
+	if (i == argc - 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc && **argv != '\0'; i++)
+		mul *= atoi(argv[i]);
+	printf("%d\n", mul);
+	return (0);
+}
