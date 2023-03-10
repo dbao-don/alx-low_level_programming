9c05db7bf7876391a1d94a5222bc4fc98ca9f2f6
Donbosco Otunga <donotunga@gmail.com>
2023-03-10 13:40:06 -0800

    Added 1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 /**
  * main - a program that prints its
  * name followed by a new line.
- * @argc - arguments counter
- * @argv - pointer array of argurments
+ * @argc: arguments counter
+ * @argv: pointer array of argurments
  * Return: 0
  */
 int main(int argc, char **argv)
 {
-	printf("%s\n",argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
