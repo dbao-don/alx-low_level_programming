3845c6bd09b98ac6258e74b616572f7d0ef9e957
Donbosco Otunga <donotunga@gmail.com>
2023-03-16 12:41:38 -0700

    Reviewed code for file 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	while (s1[j] != '\0' && j <= n)
 		j++;
-	ptr = malloc(i + j + 1);
+	ptr = malloc(sizeof(char) * (i + j + 1));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
