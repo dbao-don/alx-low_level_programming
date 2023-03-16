61001fe2bbd1fc547894991df0cd4f5baa41109b
Donbosco Otunga <donotunga@gmail.com>
2023-03-16 05:22:01 -0700

    Added new project folder 0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
+int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *malloc_checked(unsigned int b);
+
+#endif
