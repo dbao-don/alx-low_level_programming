5609e56be9010f9f25c55f2ffb78334ac58a1b30
Donbosco Otunga <donotunga@gmail.com>
2023-03-20 13:34:24 -0700

    2-print_dog.c Hotfix 1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,10 +10,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		exit(0);
+		;
 	if ((*d).name == NULL)
 		printf("Name: (nil)\n");
 	if ((*d).owner == NULL)
-		printf("Name: (nil)\n");
+		printf("Owner: (nil)\n");
+	if ((*d).age <= 0)
+		printf("Age: (nil)\n");
 	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
