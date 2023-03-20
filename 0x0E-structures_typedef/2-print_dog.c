708086520758a21a612d5c3427ba5468b98cd743
Donbosco Otunga <donotunga@gmail.com>
2023-03-20 13:55:04 -0700

    2-print_dog.c Hotfix 5

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,5 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)");
 	if ((*d).owner == NULL)
 		printf("Owner: (nil)");
-	if ((*d).age <= 0)
-		printf("Age: (nil)");
 	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
