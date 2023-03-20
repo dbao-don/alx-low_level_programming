8af326a1c90a00d2e6c9e8eaa1f402075cffac66
Donbosco Otunga <donotunga@gmail.com>
2023-03-20 07:32:52 -0700

    Added file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct that defines a don
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+};
+
+#endif
