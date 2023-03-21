c9d52eef4e3d3c00656d9eb4d7a3cf77901c9de4
Donbosco Otunga <donotunga@gmail.com>
2023-03-21 06:34:57 -0700

    4-new_dog.c Hotfix 0

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,21 +11,16 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *sec_dog;
-	char *copy_name;
-	char *copy_owner;
-	int len_name = 0;
-	int len_owner = 0;
-	int i, j;
+	char *copy_name, *copy_owner;
+	int len_name = 0, len_owner = 0, i, j;
 
 	while (name[len_name] != '\0')
 		len_name++;
 	while (owner[len_owner] != '\0')
 		len_owner++;
-
 	sec_dog = malloc(sizeof(dog_t));
 	if (sec_dog == NULL)
 		return (NULL);
-
 	copy_name = malloc((len_name + 1) * sizeof(char));
 	if (copy_name == NULL)
 	{
@@ -35,10 +30,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i < len_name; i++)
 		copy_name[i] = name[i];
 	copy_name[len_name] = '\0';
-
 	copy_owner = malloc((len_owner + 1) * sizeof(char));
 	if (copy_owner == NULL)
 	{
+		free(copy_name);
 		free(sec_dog);
 		return (NULL);
 	}
@@ -49,6 +44,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	sec_dog->name = copy_name;
 	sec_dog->age = age;
 	sec_dog->owner = copy_owner;
-
 	return (sec_dog);
 }
