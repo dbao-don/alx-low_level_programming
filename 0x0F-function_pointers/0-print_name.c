0ae1bea871d44f963ef583b6204c1ffc3d57011a
Donbosco Otunga <donotunga@gmail.com>
2023-03-22 05:31:46 -0700

    Added file 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints a name
+ * @name: name to be printed
+ * @f: function to be used
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
