5d084448c0955fb97e991e43290ccc6010242bb6
Donbosco Otunga <donotunga@gmail.com>
2023-03-22 07:46:13 -0700

    0-print_name.c Hotfix 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,10 @@
  * print_name - a function that prints a name
  * @name: name to be printed
  * @f: function to be used
- * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	(*f)(name);
+	f(name);
 }
