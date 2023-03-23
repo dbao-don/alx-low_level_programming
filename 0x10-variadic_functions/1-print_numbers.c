55e4b35f5370bb7faebdeebb931539cdb7843fd5
Donbosco Otunga <donotunga@gmail.com>
2023-03-23 07:34:33 -0700

    1-print_numbers.c Hotfix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (n != 0 || separator != NULL)
+	if (n != 0 && separator != NULL)
 	{
 		va_start(args, n);
 		for (i = 0; i < n; i++)
