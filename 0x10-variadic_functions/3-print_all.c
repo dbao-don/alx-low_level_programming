37ac9d9c785d19076f6449bf2297f2c51c8436fb
Donbosco Otunga <donotunga@gmail.com>
2023-03-23 15:34:30 -0700

    Reviewed code for 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,88 +1,49 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stddef.h>
-int c_arg(va_list arg_psd);
-int i_arg(va_list arg_psd);
-int f_arg(va_list arg_psd);
-int s_arg(va_list arg_psd);
 /**
  * print_all - a function that prints all args passed to it
  * @format: type of variable passed
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0, j;
-	char *sep_1 = "", *sep_2 = ", ";
-	var select[] = {
-		{"c", c_arg},
-		{"i", i_arg},
-		{"f", f_arg},
-		{"s", s_arg},
-		{NULL, NULL}
-	};
+	int i = 0;
+	char *str, *sep = "";
 
-	va_start(args, format);
-	while (format != NULL && format[i])
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
 	{
-		j = 0;
-		while (select[j].f != NULL)
+		while (format[i])
 		{
-			if (format[i] == *(select[j].c))
+			switch (format[i])
 			{
-				printf("%s", sep_1);
-				select[j].f(args);
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
 			}
-			j++;
+			sep = ", ";
+			i++;
 		}
-		sep_1 = sep_2;
-		i++;
 	}
-	va_end(args);
-	printf("\n");
-}
-/**
- * c_arg - a function that prints a character
- * @arg_psd: list containing possible characters
- * Return: 0
- */
-int c_arg(va_list arg_psd)
-{
-	printf("%c", va_arg(arg_psd, int));
-	return (0);
-}
-/**
- * i_arg - a function that prints an integer
- * @arg_psd: list containing a possible integer
- * Return: 0
- */
-int i_arg(va_list arg_psd)
-{
-	printf("%d", va_arg(arg_psd, int));
-	return (0);
-}
-/**
- * f_arg - a function that prints a float
- * @arg_psd: list containting a possible float
- * Return: 0
- */
-int f_arg(va_list arg_psd)
-{
-	printf("%f", va_arg(arg_psd, double));
-	return (0);
-}
-/**
- * s_arg - a function that prints a string
- * @arg_psd: list containing possible string
- * Return: 0
- */
-int s_arg(va_list arg_psd)
-{
-	char *s = va_arg(arg_psd, char *);
 
-	if (s == NULL)
-		printf("(nil)");
-	else
-		printf("%s", s);
-	return (0);
+	printf("\n");
+	va_end(list);
 }
