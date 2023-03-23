2eca8f02eb23d4f75d71e2d4163d5d20044d4f92
Donbosco Otunga <donotunga@gmail.com>
2023-03-23 15:14:03 -0700

    3-print_all.c Hotfix 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,6 @@
 #ifndef VARIADIC_FUNC_H
 #define VARIADIC_FUNC_H
 #include <stdarg.h>
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -16,5 +15,4 @@ typedef struct var
 	char *c;
 	int (*f)(va_list arg_psd);
 } var;
-
 #endif
