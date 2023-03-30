12d977fc4633e49c585a4fcea14c6a2b8b699751
Donbosco Otunga <donotunga@gmail.com>
2023-03-30 13:55:40 -0700

    Renamed main.h -> lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,5 +22,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _putchar(char c);
 
 #endif
