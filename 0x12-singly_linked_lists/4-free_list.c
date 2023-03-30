b6b486487f0545e5464ec9a667fd8650041b86fc
Donbosco Otunga <donotunga@gmail.com>
2023-03-30 15:51:06 -0700

    Added 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - a function that frees a list_t list
+ * @head: pointer to head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
