4a004688aeb9107ee1b66fcc5fe6706bbfdec761
Donbosco A Otunga <donotunga@gmail.com>
2023-06-13 21:11:45 +0300

    Modified: +Calculate length[str] --> 0x0B_malloc_free/1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,21 +11,29 @@ char *_strdup(char *str)
 {
 	char *buffer;
 	int index;
+	int length = 0;
 
 	/* Check if empty string in str */
 	if (str == NULL)
 		return (NULL);
 
+	/* Calculate the size of str */
+	while (str[length] == '\0')
+		length++;
+
 	/* Assign memory to buffer using malloc */
-	buffer = malloc(sizeof(char) * sizeof(str));
+	buffer = malloc((length * sizeof(char)) + 1);
 
 	/* If malloc fails return NULL */
 	if (buffer == NULL)
 		return (NULL);
 
-	/* duplicate str to buffer */
+	/* Duplicate str to buffer */
 	for (index = 0; str[index] != '\0'; index++)
 		buffer[index] = str[index];
+
+	/* Insert the string terminator at the end */
+	buffer[index] = '\0';
 
 	return (buffer);
 }
