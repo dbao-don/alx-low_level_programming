54fb3eee4dc3e97653a3b2e995059dea2ce8e5d4
Donbosco A Otunga <donotunga@gmail.com>
2024-01-11 23:36:48 +0300

    Added: 0x17../1-.. > 6-..

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * dlistint_len - a function that returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of element in a linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count_elem = 0;
+
+	if(h == NULL)
+		return (0);
+
+	while(h != NULL)
+	{
+		count_elem++;
+		h = h->next;
+	}
+
+	return (count_elem);
+}
